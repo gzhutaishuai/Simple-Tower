537bebe9d8fcaa6e008292058032e8ee56af127f
gzhutaishuai <107551607+gzhutaishuai@users.noreply.github.com>
2023-11-28 17:00:57 +0800

    Create Wave.cs

---
+++ Wave.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave 
+{
+    public GameObject enemyPrefab;//敌人预制体
+    public int count;//个数
+    public float rate;//生成时间间隔
+}
