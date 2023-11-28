268a34ac94b4987b09b86b813eb6fd8f6563dccb
gzhutaishuai <107551607+gzhutaishuai@users.noreply.github.com>
2023-11-28 16:57:42 +0800

    Update EnemySpawner.cs

--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -1,1 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Wave[] waves;//敌人种类的数组
+    public Transform startPosition;//生成敌人的起始点
+    public float waveRate=3.0f;//每波的间隔
+    public static int curEnemyAlive=0;
+    public Coroutine coroutine;
+    void Start()
+    {
+       coroutine=StartCoroutine(SpawnEnemy());
+    }
+    public void Stop()
+    {
+        StopCoroutine(coroutine);
+    }
+    
+    IEnumerator SpawnEnemy()
+    {
+        foreach(Wave wave in waves)
+        {
+            for(int i=0;i<wave.count;i++)
+            {
+                GameObject.Instantiate(wave.enemyPrefab, startPosition.position, Quaternion.identity); 
+                curEnemyAlive++;
+                if(i!=wave.count-1)
+                yield return new WaitForSeconds(wave.rate);
+            }
+            while(curEnemyAlive>0)
+            {
+                yield return 0;
+            }
+            yield return new WaitForSeconds(waveRate);
+        }
+        while(curEnemyAlive>0)
+        {
+            yield return 0;
+        }
+        GameManager.instance.Win();
+    }
+}
 
